918a8da06670b935fa211155fe7d6f10f6ae8655
Jiuso1 <73797418+Jiuso1@users.noreply.github.com>
2023-03-01 17:13:16 +0100

    Add files via upload

--- MargaretGUI/ventanaprincipal.cpp
+++ MargaretGUI/ventanaprincipal.cpp
@@ -1,10 +1,55 @@
 #include "ventanaprincipal.h"
 #include <QCloseEvent>
 #include <QDebug>
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QDate>
+#include <QString>
+#include <QLocale>
+#include <QMap>
 
 VentanaPrincipal::VentanaPrincipal()
 {
+    verticalLayout = new QVBoxLayout;
+    fechaLabel = new QLabel;
+    anadirPr = new QPushButton;
+    eliminarPr = new QPushButton;
+    fecha = new QDate;
+    fechaCadena = new QString;
+    transformador = new QLocale;
+    //qDebug()<<transformador->toString(12);
 
+    int dia = 0,mes = 0,anio = 0;
+    fecha->currentDate().getDate(&anio,&mes,&dia);
+    QMap<int,QString> mapa = {//Mapea el número del mes con el nombre.
+                {1,"enero" },
+                {2,"febrero" },
+                {3,"marzo" },
+                {4,"abril" },
+                {5,"mayo" },
+                {6,"junio" },
+                {7,"julio" },
+                {8,"agosto" },
+                {9,"septiembre" },
+                {10,"octubre" },
+                {11,"noviembre" },
+                {12,"diciembre" }
+    };
+
+    *fechaCadena = *fechaCadena + "A dia " + transformador->toString(dia) + " de " + mapa.take(mes) + " de " + transformador->toString(anio);
+
+    fechaLabel->setText(*fechaCadena);
+    anadirPr->setText("Añadir programa");
+    eliminarPr->setText("Eliminar programa");
+    connect(anadirPr,SIGNAL(clicked()),
+            this,SLOT(close()));
+    connect(eliminarPr,SIGNAL(clicked()),
+            this,SLOT(close()));
+    verticalLayout->addWidget(fechaLabel);
+    verticalLayout->addWidget(anadirPr);
+    verticalLayout->addWidget(eliminarPr);
+    setLayout(verticalLayout);
 }
 void VentanaPrincipal::closeEvent(QCloseEvent *event){
     qDebug()<<"Has salido de la aplicación";
--- MargaretGUI/ventanaprincipal.h
+++ MargaretGUI/ventanaprincipal.h
@@ -1,12 +1,19 @@
 #ifndef VENTANAPRINCIPAL_H
 #define VENTANAPRINCIPAL_H
 
-#include <QMainWindow>
+//#include <QMainWindow>//Vamos a usar QDialog porque es el que nos funciona.
+#include <QDialog>
 
 class QCloseEvent;
 class QLabel;
+class QPushButton;
+class QVBoxLayout;
+class QDate;
+class QLocale;
+class QString;
 
-class VentanaPrincipal : public QMainWindow
+
+class VentanaPrincipal : public QDialog
 {
     Q_OBJECT
 
@@ -15,6 +22,18 @@ public:
 
 protected:
     void closeEvent(QCloseEvent *event);
+
+private slots:
+
+
+private:
+    QLabel *fechaLabel;//Label que almacena la fecha. Formato: "A día "<<dia<<" de "<<mes.
+    QPushButton *anadirPr;//Botón que abre el diálogo para añadir un programa.
+    QPushButton *eliminarPr;//Botón que abre el diálogo para eliminar un programa.
+    QVBoxLayout *verticalLayout;//Layout en el que incluiremos los diferentes widgets.
+    QDate *fecha;
+    QLocale *transformador;
+    QString *fechaCadena;
 };
 
 #endif // VENTANAPRINCIPAL_H
